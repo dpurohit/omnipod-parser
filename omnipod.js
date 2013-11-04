5e87397ec4a8d4e1d90a597cbff6c995666dcd8d
Jamie Bate <jamie@almservices.co.nz>
2013-11-04 22:51:29 +1300

    use test xml from examples dir

--- omnipod.js
+++ omnipod.js
@@ -2,17 +2,17 @@
 var fs = require('fs'),
     es = require('event-stream'),
     StringDecoder = require('string_decoder').StringDecoder;
-var stream = fs.createReadStream('C:\\Users\\Dhwanit\\Desktop\\omnipod\\test.xml');
+var stream = fs.createReadStream('./examples/test.xml');
 var decoder = new StringDecoder('utf-8');
 var i = 0;
 es.pipeline(stream, 
-			es.split(/>\s+</),
-			es.map( function (data, cb) {
-				var dataAsString = decoder.write((data || ""));
-				if(!(/(xml|RECORD(S?))/ig.test(dataAsString))){
-					console.log("record:"+ dataAsString.replace(/\s+/g,"|"));
-				}
-				cb(null, data); // make sure you call cb.
-				i++;
-		    })
-		);
+      es.split(/>\s+</),
+      es.map( function (data, cb) {
+        var dataAsString = decoder.write((data || ""));
+        if(!(/(xml|RECORD(S?))/ig.test(dataAsString))){
+          console.log("record:"+ dataAsString.replace(/\s+/g,"|"));
+        }
+        cb(null, data); // make sure you call cb.
+        i++;
+        })
+    );
